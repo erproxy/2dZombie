c4e9f601120dc6787cd34c2471f8a5c353359c59
erproxy <69542801+erproxy@users.noreply.github.com>
2021-07-23 16:23:50 +0400

    NewVersionMenu
    Изменил версию меню, добавил лист комнат.

---
+++ 2dZombie/Assets/Skripts/Canvas/Menu.cs
@@ -0,0 +1,30 @@
+﻿using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NetCore;
+
+namespace Ui
+{
+    public class Menu : MonoBehaviour
+    {
+        [SerializeField] private GameObject holder, item;
+
+        public void UpdateRooms()
+        {
+            var rooms = FindObjectOfType<PhotonLobby>().rooms;
+            foreach (Transform item in holder.transform)
+            {
+                Destroy(item.gameObject);
+            }
+
+            print("Rooms: " + rooms.Count);
+            for (int i = 0; i < rooms.Count; i++)
+            {
+                var h = Instantiate(item, holder.transform);
+                h.GetComponent<ConnectToRoom>().text.text = rooms[i].Name;
+                h.SetActive(true);
+            }
+        }
+    }
+}
